241e02fa68082ed722fc9524eee5fd1a417cc067
teribogard <username@domain.example>
2025-01-27 17:02:31 -0500

    add form elements to registration page

--- src/main/java/com/parasoft/pages/RegistrationPage.java
+++ src/main/java/com/parasoft/pages/RegistrationPage.java
@@ -1,6 +1,46 @@
 package com.parasoft.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class RegistrationPage extends BasePage{
 
     //TODO add registration form input boxes' web elements
+
+    @FindBy(id="customer.firstName")
+    public WebElement firstName;
+
+    @FindBy(id="customer.lastName")
+    public WebElement lastName;
+
+    @FindBy(id="customer.address.street")
+    public WebElement address;
+
+    @FindBy(id="customer.address.city")
+    public WebElement city;
+
+    @FindBy(id="customer.address.state")
+    public WebElement state;
+
+    @FindBy(id="customer.address.zipCode")
+    public WebElement zipcode;
+
+    @FindBy(id="customer.phoneNumber")
+    public WebElement phone;
+
+    @FindBy(id="customer.ssn")
+    public WebElement ssn;
+
+    @FindBy(id="customer.username")
+    public WebElement usernameInput;
+
+    @FindBy(id="customer.password")
+    public WebElement passwordInput;
+
+    @FindBy(id="repeatPassword")
+    public WebElement confirmPasswordInput;
+
+    @FindBy(css="input[value='Register']")
+    public WebElement registerButton;
+
 }
